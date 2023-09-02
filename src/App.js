a847ac82745873baf2f98041932b41df6eefda10
Faizan <faizan.rauf@ercot.com>
2023-09-02 12:23:37 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>here is REACT_APP_TEST_ENV_VAR value: {process.env.REACT_APP_TEST_ENV_VAR}</p>
+        <p>here is API_URL value: {process.env.API_URL}</p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
